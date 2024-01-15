34ee2e675e403eb2be1b725b9c46e31fc2cea83b
Brendon Lee <144931412+bRENDON-lEE10@users.noreply.github.com>
2024-01-14 17:33:24 -0700

    Add files via upload

---
+++ Wall Collide Challenge/main.js
@@ -0,0 +1,84 @@
+// Canvas Sandbox
+
+// Canvas Setup
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Global Variables
+let player = {
+  x: 400,
+  y: 300,
+  w: 25,
+  h: 25,
+  speed: 5,
+  color: "blue",
+};
+
+let leftPressed = false;
+let rightPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+// Draw Functions
+window.addEventListener("load", draw);
+
+function draw() {
+  // Logic
+  if (rightPressed) {
+    player.x += player.speed;
+  } else if (leftPressed) {
+    player.x += -player.speed;
+  } else if (upPressed) {
+    player.y += -player.speed;
+  } else if (downPressed) {
+    player.y += player.speed;
+  }
+}
+
+// Drawing
+
+  // Backround
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  ctx.fillStyle = "grey";
+ctx.fillRect( 500,250,50,150)
+
+  // Draw Player
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.w, player.h);
+  requestAnimationFrame(draw);
+
+
+  // Event Stuff
+document.addEventListener("keydown", keydownHandler);
+
+function keydownHandler(e) {
+  console.log(e.code);
+  if (e.code === "ArrowLeft") {
+    leftPressed = true;
+  } else if (e.code === "ArrowRight") {
+    rightPressed = true;
+  } else if (e.code === "ArrowUp") {
+    upPressed = true;
+  } else if (e.code === "ArrowDown") {
+    downPressed = true;
+  }
+}
+
+document.addEventListener("keyup", keyupHandler);
+
+function keyupHandler(e) {
+  console.log(e.code);
+  if (e.code === "ArrowLeft") {
+    leftPressed = false;
+  } else if (e.code === "ArrowRight") {
+    rightPressed = false;
+  } else if (e.code === "ArrowUp") {
+    upPressed = false;
+  } else if (e.code === "ArrowDown") {
+    downPressed = false;
+  }
+}
