112f7107051353afd963099642a97879296b8748
Brendon Lee <144931412+bRENDON-lEE10@users.noreply.github.com>
2024-01-18 09:33:11 -0700

    Update main.js

--- Wall Collide Challenge/main.js
+++ Wall Collide Challenge/main.js
@@ -1,87 +1,142 @@
-// Canvas Sandbox
+//Play with Player Movement
 
-// Canvas Setup
+//Set up Canvas and Grapics Context
+
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
-// Global Variables
+//Global Variables
 let player = {
+  x: 75,
+  y: 392,
+  w: 30,
+  h: 30,
+  // xSpeed: 0,
+  // ySpeed: 0,
+  speed: 3,
+};
+let rectangle = {
   x: 400,
   y: 300,
-  w: 25,
-  h: 25,
-  speed: 5,
-  color: "blue",
+  w: 45,
+  h: 200,
 };
 
-let leftPressed = false;
-let rightPressed = false;
-let upPressed = false;
-let downPressed = false;
+let ArrowRightPresssed = false;
+let ArrowLeftPressed = false;
+let ArrowUpPressed = false;
+let ArrowDownPressed = false;
+let KeyD = false;
+let KeyA = false;
+let KeyW = false;
+let KeyS = false;
+let KeyQpressed = false;
 
-// Draw Functions
-window.addEventListener("load", draw);
+// Main progrm loop
+requestAnimationFrame(draw);
 
 function draw() {
-  // Logic
-
-  // Move Blue Player
-  if (rightPressed) {
+  //Move playerBlue based on what key is pressed / held down
+  if (ArrowRightPresssed) {
     player.x += player.speed;
-  } else if (leftPressed) {
+  } else if (ArrowLeftPressed) {
     player.x += -player.speed;
-  } else if (upPressed) {
+  }
+
+  if (ArrowUpPressed) {
     player.y += -player.speed;
-  } else if (downPressed) {
+  } else if (ArrowDownPressed) {
     player.y += player.speed;
   }
 
-  // Drawing
+  // Check collision
+  if (
+    player.x + player.w > rectangle.x &&
+    player.x < rectangle.x + rectangle.w &&
+    player.y + player.h > rectangle.y &&
+    player.y < rectangle.y + rectangle.h
+  ) {
+    player.x = 75;
+  }
 
-  // Backround
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, cnv.width, cnv.height);
+  if (KeyQpressed) {
+    rectangle.x = Math.random() * 600 + 100;
+    rectangle.y = Math.random() * 300 + 100;
+  }
 
-  ctx.fillStyle = "grey";
-  ctx.fillRect(500, 250, 50, 150);
+  //Drawing
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-  // Draw Player
-  ctx.fillStyle = player.color;
+  //Draw Player
+  ctx.fillStyle = "blue";
   ctx.fillRect(player.x, player.y, player.w, player.h);
 
-  // Animation Loop
+  //Draw rectangle
+  ctx.fillStyle = "grey";
+  ctx.fillRect(rectangle.x, rectangle.y, rectangle.w, rectangle.h);
+
+  //Request another Animation Frame
   requestAnimationFrame(draw);
 }
 
-// Event Stuff
-document.addEventListener("keydown", keydownHandler);
+//Key Event Stuff
 
-function keydownHandler(e) {
-  console.log(e.code);
-  if (e.code === "ArrowLeft") {
-    leftPressed = true;
-  } else if (e.code === "ArrowRight") {
-    rightPressed = true;
-  } else if (e.code === "ArrowUp") {
-    upPressed = true;
-  } else if (e.code === "ArrowDown") {
-    downPressed = true;
+document.addEventListener("keydown", keydownHandler);
+document.addEventListener("keyup", keyupHandler);
+
+function keydownHandler(event) {
+  //KeyIsPressed movement Blue
+  if (!event.repeat) {
+    if (event.code == "ArrowRight") {
+      ArrowRightPresssed = true;
+    } else if (event.code == "ArrowLeft") {
+      ArrowLeftPressed = true;
+    } else if (event.code == "ArrowUp") {
+      ArrowUpPressed = true;
+    } else if (event.code == "ArrowDown") {
+      ArrowDownPressed = true;
+    } else if (event.code == "KeyQ") {
+      KeyQpressed = true;
+    }
+  }
+
+  //check canvas boundaries
+  if (player.x > 770) {
+    player.x = 75;
+  } else if (player.x < -20) {
+    player.x = 700;
+  } else if (player.y < 0) {
+    player.y = 600;
+  } else if (player.y > 600) {
+    player.y = 0;
   }
 }
 
-document.addEventListener("keyup", keyupHandler);
+function keyupHandler(event) {
+  if (event.code == "ArrowRight") {
+    ArrowRightPresssed = false;
+  } else if (event.code == "ArrowLeft") {
+    ArrowLeftPressed = false;
+  } else if (event.code == "ArrowUp") {
+    ArrowUpPressed = false;
+  } else if (event.code == "ArrowDown") {
+    ArrowDownPressed = false;
+  } else if (event.code == "KeyQ") {
+    KeyQpressed = false;
+  }
+  //KeyIsPressed movement Blue
 
-function keyupHandler(e) {
-  console.log(e.code);
-  if (e.code === "ArrowLeft") {
-    leftPressed = false;
-  } else if (e.code === "ArrowRight") {
-    rightPressed = false;
-  } else if (e.code === "ArrowUp") {
-    upPressed = false;
-  } else if (e.code === "ArrowDown") {
-    downPressed = false;
+  if (event.code == "KeyD") {
+    KeyD = false;
+  } else if (event.code == "KeyA") {
+    KeyA = false;
+  } else if (event.code == "KeyW") {
+    KeyW = false;
+  } else if (event.code == "KeyS") {
+    KeyS = false;
+  } else if (event.code == "KeyQ") {
+    KeyQpressed = false;
   }
 }
